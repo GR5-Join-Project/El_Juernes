a4c31eb9ae172d8b9f177186669adf7164673e65
adricasi <adriator6@gmail.com>
2018-04-01 02:30:49 +0200

    Mostrar al redactor les notícies que té assignades

--- El_JuernesApp/AfeNews/urls.py
+++ El_JuernesApp/AfeNews/urls.py
@@ -1,10 +1,10 @@
+from AfeNews import views
+from AfeNews.views import Afe_News_List, News_assigned
 from django.conf.urls import url
 from django.urls import path
 
-from AfeNews import views
-from AfeNews.views import Afe_News_List
-
 urlpatterns = [
     path('', Afe_News_List, name="AFE"),
+    path('assigned/', News_assigned, name="assigned"),
     url(r'^new/(?P<slug>[\w-]+)/$', views.full_new_and_assignations.as_view(), name='full_new_and_assignations')
 ]
